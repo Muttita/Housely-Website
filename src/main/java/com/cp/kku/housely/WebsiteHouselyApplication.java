1e491a62dab59888562c9826410a99c6aaad5337
Muttita <muttita.c@kkumail.com>
2024-09-23 19:42:45 +0700

    up

---
+++ src/main/java/com/cp/kku/housely/WebsiteHouselyApplication.java
@@ -0,0 +1,13 @@
+package com.cp.kku.housely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteHouselyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteHouselyApplication.class, args);
+	}
+
+}
