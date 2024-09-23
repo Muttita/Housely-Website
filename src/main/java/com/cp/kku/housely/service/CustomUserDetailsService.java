9ec94800f11ffd34ae5b369a0fb12f64a650fe73
user-gp1 <narongrit.pa@kkumail.com>
2024-09-24 05:01:52 +0700

    login

--- src/main/java/com/cp/kku/housely/service/CustomUserDetailsService.java
+++ src/main/java/com/cp/kku/housely/service/CustomUserDetailsService.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
-
+//
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
