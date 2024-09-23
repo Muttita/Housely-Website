6896641272324dee776ac607435bd80060673a3a
user-gp1 <narongrit.pa@kkumail.com>
2024-09-24 04:57:16 +0700

    login

--- src/main/java/com/cp/kku/housely/service/CustomUserDetailsService.java
+++ src/main/java/com/cp/kku/housely/service/CustomUserDetailsService.java
@@ -2,12 +2,17 @@ package com.cp.kku.housely.service;
 
 import com.cp.kku.housely.model.User;
 import com.cp.kku.housely.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -24,5 +29,11 @@ public class CustomUserDetailsService implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
 
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(List<String> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
     }
 }
