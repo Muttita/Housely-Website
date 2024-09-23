32fea0b6d15e26d37a5afe23e4e28545cd6e2e3d
user-gp1 <narongrit.pa@kkumail.com>
2024-09-24 05:55:58 +0700

    login

--- src/main/java/com/cp/kku/housely/service/CustomUserDetailsService.java
+++ src/main/java/com/cp/kku/housely/service/CustomUserDetailsService.java
@@ -3,18 +3,11 @@ package com.cp.kku.housely.service;
 import com.cp.kku.housely.model.User;
 import com.cp.kku.housely.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -25,12 +18,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        // Map role to a SimpleGrantedAuthority and add "ROLE_" prefix
-        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + user.getRole());
-
-        // Return Spring Security's User object with authorities
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), Collections.singletonList(authority));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                AuthorityUtils.createAuthorityList(user.getRole())
+        );
     }
 }
